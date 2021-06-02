720d27424b0f39b31cf5fcd956f8b13c20e39091
wxchencn <chenwenxuanhehe@gmial.com>
2021-06-02 16:12:10 +0800

    update

---
+++ docs/config.js
@@ -0,0 +1,12 @@
+/*
+ * @Description:
+ * @Version: 1.0
+ * @Autor: wxchen
+ * @Date: 2021-05-13 10:34:29
+ * @LastEditTime: 2021-05-13 10:40:13
+ */
+
+module.exports = {
+    title: 'Hello VuePress',
+    description: 'Just playing around'
+}
---
+++ docs/config.md
@@ -0,0 +1,7 @@
+<!--
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: wxchen
+ * @Date: 2021-05-13 11:02:54
+ * @LastEditTime: 2021-05-13 11:02:55
+-->
